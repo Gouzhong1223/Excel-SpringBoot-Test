32f06bc8b990c84d4b9801aac288a2952e5f46bb
Gouzhong1223 <1162864960@qq.com>
2020-10-29 23:01:51 +0800

    init

---
+++ src/main/java/com/gouzhong1223/excelspeingboot/ExcelSpeingbootApplication.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.excelspeingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelSpeingbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelSpeingbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gouzhong1223/excelspeingboot/ExcelSpeingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.excelspeingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelSpeingbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
